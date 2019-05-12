f1967010df9b5f5ac41fae566772f24efc650132
navid <navidjafarof@gmail.com>
2019-05-12 23:20:56 +0430

    Print Board Updated!

--- Minimax.py
+++ Minimax.py
@@ -2,7 +2,7 @@ from math import inf as infinity
 from random import choice
 
 HUMAN = -1
-COMP = 1
+COMPUTER = 1
 
 board = [
     [0, 0, 0],
@@ -15,16 +15,13 @@ def print_board(state, c_choice, h_choice):
     chars = {
         -1: h_choice,
         +1: c_choice,
-        0: ' '
+        0: '-'
     }
-    str_line = '---------------'
-
-    print(str_line)
     for row in state:
         for cell in row:
             symbol = chars[cell]
-            print(f'| {symbol} |', end='')
-        print('\n' + str_line)
+            print(" " + symbol + " ", end='')
+        print('\n')
 
 
 def wins(state, player):
@@ -47,7 +44,7 @@ def wins(state, player):
 
 
 def evaluate_state(state):
-    if wins(state, COMP):
+    if wins(state, COMPUTER):
         score = +1
     elif wins(state, HUMAN):
         score = -1
@@ -57,7 +54,7 @@ def evaluate_state(state):
 
 
 def game_over(state):
-    return wins(state, HUMAN) or wins(state, COMP) or is_full()
+    return wins(state, HUMAN) or wins(state, COMPUTER) or is_full()
 
 
 def empty_cells(state):
@@ -91,24 +88,24 @@ def set_move(x, y, player):
         return False
 
 
-def minimax(state, depth, player):
-    if player == COMP:
+def min_max(state, depth, player):
+    if player == COMPUTER:
         best = [-1, -1, -infinity]
     else:
         best = [-1, -1, +infinity]
 
-    if depth == 0 or game_over(state):
+    if game_over(state):
         score = evaluate_state(state)
         return [-1, -1, score]
 
     for cell in empty_cells(state):
         x, y = cell[0], cell[1]
         state[x][y] = player
-        score = minimax(state, depth - 1, -player)
+        score = min_max(state, depth - 1, -player)
         state[x][y] = 0
         score[0], score[1] = x, y
 
-        if player == COMP:
+        if player == COMPUTER:
             if score[2] > best[2]:
                 best = score  # max value
         else:
@@ -129,10 +126,10 @@ def ai_turn(c_choice, h_choice):
         x = choice([0, 1, 2])
         y = choice([0, 1, 2])
     else:
-        move = minimax(board, len(empty_cells(board)), COMP)
+        move = min_max(board, len(empty_cells(board)), COMPUTER)
         x, y = move[0], move[1]
 
-    set_move(x, y, COMP)
+    set_move(x, y, COMPUTER)
 
 
 def human_turn(c_choice, h_choice):
@@ -188,7 +185,7 @@ def main():
         print(f'Human turn [{h_choice}]')
         print_board(board, c_choice, h_choice)
         print('YOU WIN!')
-    elif wins(board, COMP):
+    elif wins(board, COMPUTER):
         print(f'Computer turn [{c_choice}]')
         print_board(board, c_choice, h_choice)
         print('YOU LOSE!')
